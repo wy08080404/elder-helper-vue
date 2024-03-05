c3e8dffb2098903eec9d08b3132aa34cab4dd8ad
wy08080404 <vivyan040808@outlook.com>
2024-03-05 20:42:25 +0800

    初稿
    ”我的“页面和首页初稿（求助页面）

---
+++ elder-helper-vue/pages/firstPage/firstPage.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<img class = "userPortrait" :src="imgSrc"/>
+		<view>
+			<button>打开弹窗</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				imgSrc:"../../static/elderUser.png",
+				show:false
+			};
+		},
+		methods:{
+		}
+
+		
+	}
+</script>
+
+<style lang="scss">
+.userPortrait{
+	width: 400rpx;
+	border-radius: 100%;
+	display: block;
+	margin-top: 100rpx;
+	margin-left: auto;
+	margin-right: auto;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+	
+}
+</style>
